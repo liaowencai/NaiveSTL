14b88b2f6828f0af479fd97967fa991893d3f166
liaowencai <liaowencai518@qq.com>
2017-03-20 10:49:13 +0800

    add iterator adapters: inserter_iterator, iostream iterator, reverse iterator

--- naive_stl_iterator.h
+++ naive_stl_iterator.h
@@ -5,7 +5,8 @@
 #ifndef NAIVE_STL_ITERSTOR_H
 #define NAIVE_STL_ITERSTOR_H
 
-#include<cstddef>
+#include <cstddef>
+#include <iostream>
 
 // 5种迭代器
 struct input_iterator_tag {};
@@ -158,4 +159,250 @@ inline void advance(InputIterator& i, Distance n)
     __advance(i, n, iterator_category(i));
 }
 
-#endif
+
+//-----------------------------------------------------------------------
+// iterator adapters
+//-----------------------------------------------------------------------
+template <class Container>
+class back_insert_iterator
+{
+protected:
+	Container* container;
+public:
+	typedef output_iterator_tag iterator_category;
+	typedef void				value_type;
+	typedef void				difference_type;
+	typedef void				pointer;
+	typedef void				reference;
+
+	explicit back_insert_iterator(Container& x) : container(&x) {}
+	back_insert_iterator<Container>&
+		operator=(const typename Container::value_type& value)
+	{	// 转而调用push_back
+		container->push_back(value);
+		return *this;
+	}
+	// 关闭下列三个功能
+	back_insert_iterator<Container>& operator*() { return *this; }
+	back_insert_iterator<Container>& operator++() { return *this; }
+	back_insert_iterator<Container>& operator++(int) { return *this; }
+};
+template <class Container>
+inline back_insert_iterator<Container> back_inserter(Container& x)
+{
+	return back_insert_iterator<Container>(x);
+}
+
+template <class Container>
+class front_insert_iterator
+{
+protected:
+	Container* container;
+public:
+	typedef output_iterator_tag iterator_category;
+	typedef void				value_type;
+	typedef void				difference_type;
+	typedef void				pointer;
+	typedef void				reference;
+
+	explicit front_insert_iterator(Container& x) : container(&x) {}
+	front_insert_iterator<Container>&
+		operator=(const typename Container::value_type& value)
+	{	// 转而调用push_front
+		container->push_front(value);
+		return *this;
+	}
+	// 关闭下列三个功能
+	front_insert_iterator<Container>& operator*() { return *this; }
+	front_insert_iterator<Container>& operator++() { return *this; }
+	front_insert_iterator<Container>& operator++(int) { return *this; }
+};
+template <class Container>
+inline front_insert_iterator<Container> front_inserter(Container& x)
+{
+	return front_insert_iterator<Container>(x);
+}
+
+template <class Container>
+class insert_iterator
+{
+protected:
+	Container* container;
+	typename Container::iterator iter;
+public:
+	typedef output_iterator_tag iterator_category;
+	typedef void				value_type;
+	typedef void				difference_type;
+	typedef void				pointer;
+	typedef void				reference;
+
+	insert_iterator(Container& x, typename Container::iterator i) : container(&x), iter(i) {}
+	insert_iterator<Container>&
+		operator=(const typename Container::value_type& value)
+	{	
+		iter = container->insert(iter, value);
+		++iter;		// insert iterator永远随着目标移动
+		return *this;
+	}
+	// 关闭下列三个功能
+	insert_iterator<Container>& operator*() { return *this; }
+	insert_iterator<Container>& operator++() { return *this; }
+	insert_iterator<Container>& operator++(int) { return *this; }
+};
+template <class Container， class Iterator>
+inline insert_iterator<Container> inserter(Container& x, Iterator i)
+{
+	typedef typename Container::iterator iter;
+	return insert_iterator<Container>(x, iter(i));
+}
+
+//---------------------------------------------------------------------------------------
+//
+template <class Iterator>
+class reverse_iterator
+{
+protected:
+	Iterator current;
+public:
+	typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+	typedef typename iterator_traits<Iterator>::value_type		  value_type;
+	typedef typename iterator_traits<Iterator>::difference_type	  difference_type;
+	typedef typename iterator_traits<Iterator>::pointer			  pointer;
+	typedef typename iterator_traits<Iterator>::reference		  reference;
+
+	typedef Iterator iterator_type;
+	typedef reverse_iterator<Iterator> self;
+public:
+	reverse_iterator() {}
+	explicit reverse_iterator(iterator_type x) : current(x) {}
+	reverse_iterator(const self& x) : current(x.current) {}
+
+	iterator_type base() const { return current; }
+	reference operator*() const
+	{
+		Iterator tmp = current;
+		// 对逆向迭代器取值，就是将对应的正向迭代器后退一格而后取值
+		return *--tmp;
+	}
+	pointer operator->() const { return &(operator*()); }
+	// 前进++变成后退--
+	self& operator++()
+	{
+		--current;
+		return *this;
+	}
+	self operator++(int)
+	{
+		self tmp = *this;
+		--current;
+		return tmp;
+	}
+	self& operator--()
+	{
+		++current;
+		return *this;
+	}
+	self operator--(int)
+	{
+		self tmp = *this;
+		++current;
+		return tmp;
+	}
+	self operator+(difference_type n) const
+	{
+		return self(current - n);
+	}
+	self& operator+=(difference_type n)
+	{
+		current -= n;
+		return *this;
+	}
+	self operator-(difference_type n) const
+	{
+		return self(current + n);
+	}
+	self& operator-=(difference_type n)
+	{
+		current += n;
+		return *this;
+	}
+
+	reference operator[](difference_type n) const
+	{
+		return *(*this + n);
+	}
+};
+
+template <class T, class Distance = ptrdiff_t>
+class istream_iterator
+{
+	friend bool operator==(const istream_iterator<T, Distance>& x,
+		const istream_iterator<T, Distance>& y);
+protected:
+	istream* stream;
+	T value;
+	bool end_marker;
+	void read()
+	{
+		end_marker = (*stream) ? true : false;
+		if (end_marker)
+			*stream >> value;
+		end_marker = (*stream) ? true : false;
+	}
+public:
+	typedef input_iterator_tag	iterator_category;
+	typedef T					value_type;
+	typedef Distance			distance_type;
+	typedef const T*			pointer;
+	typedef const T&			reference;
+
+	istream_iterator() : stream(&cin), end_marker(false) {}
+	istream_iterator(istream& s) : stream(&s) { read(); }
+
+	reference operator*() const { return value; }
+	pointer operator->() const { return &(operator*()); }
+
+	istream_iterator<T, Distance>& operator++()
+	{
+		read();
+		return *this;
+	}
+	istream_iterator<T, Distance> operator++(int)
+	{
+		istream_iterator<T, Distance> tmp = *this;
+		read();
+		return tmp;
+	}
+};
+
+template <class T>
+class ostream_iterator
+{
+protected:
+	ostream* stream;
+	const char* string;
+
+public:
+	typedef output_iterator_tag iterator_category;
+	typedef void				value_type;
+	typedef void				difference_type;
+	typedef void				pointer;
+	typedef void				reference;
+
+	ostream_iterator(ostream& s) : stream(&s), string(nullptr) {}
+	ostream_iterator(ostream& s, const char* c) : stream(&s), string(c) {}
+
+	ostream_iterator<T>& operator=(const T& value)
+	{
+		*stream << value;
+		if (string)
+			*stream << string;
+		return *this;
+	}
+	ostream_iterator<T>& operator*() { return *this; }
+	ostream_iterator<T>& operator++() { return *this; }
+	ostream_iterator<T>& operator++(int) { return *this; }
+};
+
+
+#endif // NAIVE_STL_ITERATOR_H
