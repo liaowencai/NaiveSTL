d35b66865a65e757f3257b50a23f4441b92ad2fe
liaowencai <liaowencai518@qq.com>
2017-03-20 10:46:17 +0800

    add file "naive_algorithm.h"

--- naive_algorithm.h
+++ naive_algorithm.h
@@ -1,1 +1,9 @@
-#pragma once
+#ifndef NAIVE_ALGORITHM_H
+#define NAIVE_ALGORITHM_H
+
+#include "naive_stl_heap.h"
+#include "naive_stl_algobase.h"
+#include "naive_stl_algo.h"
+
+#endif // !NAIVE_ALGORITHM_H
+
